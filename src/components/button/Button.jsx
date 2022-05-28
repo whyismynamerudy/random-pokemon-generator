1e727e91d6e8b84af4da623193e423040c2cbcdc
Rudy <rudraksh.monga@mail.utoronto.ca>
2022-05-28 14:12:07 +0530

    added css

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
+import "./Button.css"
 
 const Button = (props) => {
 
   const my_name = JSON.stringify(props.name).replace(/"/g, '');
-  const is_vowel = "aeiou".indexOf(my_name[0]) != -1;
+  const is_vowel = "aeiou".indexOf(my_name[0]) !== -1;
   
   if (!is_vowel) {
     return (
         <div className="button">
-            <p>this is a {my_name}</p>
-            <button className="button-button" onClick={props.onClick}>generate a random pokemon</button>
+            <p>This is a <b>{my_name}</b>.</p>
+            <button className="button-button" onClick={props.onClick}>Generate a random Pokemon!</button>
         </div>
       )
   }
   return (
     <div className="button">
-        <p>this is an {my_name}</p>
-        <button className="button-button" onClick={props.onClick}>generate a random pokemon</button>
+        <p>This is an <b>{my_name}</b>.</p>
+        <button className="button-button" onClick={props.onClick}>Generate a random Pokemon!</button>
     </div>
   )
 }
